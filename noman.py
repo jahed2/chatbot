63036003b3ec559d01c5b23b6b31a005aba8e944
jahed2 <48939978+jahed2@users.noreply.github.com>
2020-05-15 09:48:29 +0600

    Add files via upload

--- noman.py
+++ noman.py
@@ -5,21 +5,37 @@ from chatterbot.trainers import ListTrainer
 from _tkinter import *
 bot=chatterbot.ChatBot("my bot")
 convo=[
-    'hello',
-   'hey there!',
-    'what is your name?',
-    'my name is Bot,i am created by noman',
-    'how are you?',
-    'i am doing great these days',
-    'thanktyou',
-    'In which city you live?',
-    'I live in dhaka',
-    'In which language you talk?',
-    'I most talk in english'
-    'what is your favourite hobby?',
-    'my favourite hobby is playing cricket ',
-    'what is your profession?',
-    'i am student from eastwest university'
+    'what is coronavirus',
+   'Coronaviruses are a large family of viruses which may cause illness in animals or humans',
+    'what is COVID-19',
+    'COVID-19 is the infectious disease caused by the most recently discovered coronavirus.',
+    'what are the symptoms of COVID-19',
+    'fever,dry cough,tiredness',
+    'who is risk for coronavirus?',
+    'these are older people and who are underlying medical condition'
+    'what should i do if i have COVID-19 symptoms?',
+    'stay home,isolate from other,take who guideness',
+    'How can we protect others',
+    'if possible,maintain minimum 1 meter distance',
+    'what should i do if i have close contact with someone who are COVID-19 affected?'
+    'if you are not live in dengue infected area you can follow minimum 14 days self quarintine yourself ',
+    'what does mean self-quarantine?',
+    'Quarantine means restricting activities or separating people who are not ill themselves',
+    'what does mean physical distancing?',
+    'minimum 1 meter distance from other'
+    'what does it mean to self isolate? ',
+    'keep at least one meter from other and 14 days isolate from other',
+    'is there any vaccine drug or treatment?',
+    'clean your hand frequently and througly, Avoid touching your nose and mouth, keep minimum 1 meters distance ',
+    'Does who recommand medical mask for prevenet COVID-19?',
+    'WHO recomend N95 and sergical mask for covid-19',
+    'how long does the virus survive on surfaces?',
+    'COVID-19 virus can survive for up to 72 hours on plastic and stainless steel,',
+    'how can protect myself from covid-19?',
+    'wash hand minimum 20 second , keep distance minimum 1 meter and use mask'
+
+
+
 
 
 ]
@@ -37,7 +53,7 @@ trainer.train(convo)
     #print("bot : ",answer)
 main = Tk()
 main.geometry("500x650")
-main.title("my chatboot")
+main.title("covid-19 chatbot")
 img=PhotoImage(file="bot2.png")
 photoL=Label(main,image=img)
 photoL.pack(pady=5)
@@ -51,7 +67,7 @@ def ask_from_bot():
 
 frame=Frame(main)
 sc=Scrollbar(frame)
-msgs=Listbox(frame,width=80,height=20)
+msgs=Listbox(frame,width=150,height=20)
 sc.pack(side=RIGHT,fill=Y)
 msgs.pack(side=LEFT,fill=BOTH,pady=10)
 frame.pack()
